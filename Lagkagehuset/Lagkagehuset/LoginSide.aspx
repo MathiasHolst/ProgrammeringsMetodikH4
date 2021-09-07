4df9a18998065a844976322f23a936e500bfb3c6
Jeppe Bjelke Knudsen Vad <jbva.skp@edu.mercantec.dk>
2021-09-07 13:08:50 +0200

    loginside

---
+++ Lagkagehuset/Lagkagehuset/LoginSide.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginSide.aspx.cs" Inherits="Lagkagehuset.LoginSide" %>
+
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+    <div class="loginDiv" >
+    <form id="loginForm">
+        <div class="container">
+            <label>Brugernavn: </label>
+            <input type="text" placeholder="Indsæt Brugernavn  " name="username" required="required"/>
+            <br /> <br />
+            <label>Kodeord: </label>
+            <input type="password" placeholder="Indsæt Kodeord" name="password" required="required" />
+            <br /><br />
+            <button type="submit" onclick="loginClicked" onserverClick="loginClicked">Log ind</button>  
+        </div>
+    </form>
+    </div>
+    <style type="text/css">
+        .loginDiv {position:fixed; top:50%; left:50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%);}
+    </style>
+</body>
---
+++ Lagkagehuset/Lagkagehuset/LoginSide.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lagkagehuset
+{
+    public partial class LoginSide : System.Web.UI.Page
+    {
+        public static List<users> allusers = new List<users>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            users medarbejder = new users();
+            //List<users> allusers = new List<users>();
+            createUser(medarbejder, 2, "medarbejder", "medarbejder", "Ian", "ian@ian.dk", 12312323, true);
+            allusers.Add(medarbejder);
+            if (Request.QueryString["username"] == medarbejder.username.ToString() && Request.QueryString["password"] == medarbejder.password.ToString())
+            {
+                Server.Transfer("Default.aspx");
+            }
+        }
+        public void createUser(users user, int id, string uname, string password, string name, string email, int phoneNumb, bool emp)
+        {
+
+            user.ID = id;
+            user.username = uname;
+            user.password = password;
+            user.name = name;
+            user.email = email;
+            user.phoneNumber = phoneNumb;
+            user.employee = emp;
+        }
+    }
+    public class users
+    {
+        public int ID;
+        public string username;
+        public string password;
+        public string name;
+        public string email;
+        public int phoneNumber;
+        public bool employee;
+        List<users> usersList = new List<users>();
+    }
+}
